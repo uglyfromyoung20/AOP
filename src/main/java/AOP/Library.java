15de7d2c6f8241b199070aed51ae6d5145f7a447
altyshkin <29455komandor@mail.ru>
2022-02-02 16:12:36 +0400

    Добавлена аннотация @Before которая выполняет метод до метода с основной логикой

--- src/main/java/AOP/Library.java
+++ src/main/java/AOP/Library.java
@@ -1,5 +1,8 @@
 package AOP;
 
+import org.springframework.stereotype.Component;
+
+@Component("libraryBean")
 public class Library {
     public void getBook(){
         System.out.println("Мы берем книгу");
--- src/main/java/AOP/LoggingAspect.java
+++ src/main/java/AOP/LoggingAspect.java
@@ -1,12 +1,14 @@
 package AOP;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect {
-    public void beforeGetBookAdvaice(){
+    @Before("execution(public  void getBook())")
+    public void beforeGetBookAdvice(){
         System.out.println("Попытка взять книгу");
     }
 }
